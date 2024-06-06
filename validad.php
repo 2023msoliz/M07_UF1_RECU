285dca35c3458a375e5dc40239c4f9b01ded045b
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-06-06 15:25:24 +0200

    fisrt commit

---
+++ validad.php
@@ -0,0 +1,12 @@
+<?php
+
+$llistaUsuaris = array("joan", "anna", "carlos", "maria", "carla");
+
+$sessio_nomusuari = $_POST['usuari'];
+$sessio_accepta = assert($_POST['condicions']) ? 1 : 0;
+
+
+for($i = 0; $i < count($llistaUsuaris); $i++) {
+}
+
+?>
