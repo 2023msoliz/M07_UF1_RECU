e571d0a8208fc3c1eac45246f4adf1a2966ee3c5
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2024-06-06 16:14:03 +0200

    second commit

---
+++ resultat.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Hola. " . $sessio_usuari . "<br>";
+echo "<p>Has acceptat les condicions d'us</p>";
+echo "<a href='login.html'>Tornar</a>";
+?>
--- validad.php
+++ validad.php
@@ -1,12 +1,17 @@
 <?php
 
 $llistaUsuaris = array("joan", "anna", "carlos", "maria", "carla");
-
 $sessio_nomusuari = $_POST['usuari'];
+$sessio_login = $_POST['login'] = true;
 $sessio_accepta = assert($_POST['condicions']) ? 1 : 0;
 
 
 for($i = 0; $i < count($llistaUsuaris); $i++) {
+    if ($llistaUsuaris[$i] == $sessio_nomusuari) {
+        header('Location: resultat.php');
+    } else {
+        include "login.html";
+        echo "Usuari no trobat";
+    }
 }
-
 ?>
